6743db893f61c6b068a0dc4edff3208bca00248e
ymatin <MatinYA@lad24.ru>
2019-10-02 15:54:22 +0300

    добавление Hapi сервера

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
-console.log('test');
+import {Server, Request, ResponseToolkit} from '@hapi/hapi';
+
+const init = async () => {
+
+    const server = new Server({
+        port: 4000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/test',
+        handler: (request: Request, h: ResponseToolkit) => {
+            return {message: 'hello'};
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init().then();
